1eeb054357df27591691e01d635a10e0219e468f
Rupali Meshram <meshram.rupali86@gmail.com>
2023-07-25 17:48:21 -0400

    counter app code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {  useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+ 
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount -1)}> - </button>
+      <h1> { counter } </h1>
+      <button onClick={() => setCounter((prevCount) => prevCount +1)}> + </button>
+    </div>
+  );
+}
+
+export default App;
